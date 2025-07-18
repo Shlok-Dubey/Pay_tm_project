f656579e111a71fa866dc4538247fc714b7bbd61
Shlok-Dubey <dubeyshlok12@gmail.com>
2025-07-19 00:22:15 +0530

    done passport setup

---
+++ src/config/passportConfig.ts
@@ -0,0 +1,20 @@
+import passport from 'passport';
+import { Strategy } from 'passport-google-oauth20';
+export default function intialise(){
+    passport.use(new Strategy({
+        clientID: process.env.GOOGLE_CLIENT_ID!,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+        callbackURL: "http://localhost:3000/google/auth"
+    },
+        async function (accessToken, refreshToken, profile, done) {
+            const user = {
+                googleId: Number(profile.id),
+                firstName: profile.name?.givenName,
+                lastName: profile.name?.familyName
+            };
+            return done(null, user)
+    
+        }
+    ));
+}
+
--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,10 @@ import cookieParser from 'cookie-parser'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import { connection } from './db';
+import intialise from './config/passportConfig';
 
 dotenv.config()
-
+intialise()
 const app = express();
 app.use(cors());
 app.use(express.json());
