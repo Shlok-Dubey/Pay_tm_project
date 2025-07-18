9202c0deac8e4373a38f4afbb5aed6ba35c76b8a
Shlok-Dubey <dubeyshlok12@gmail.com>
2025-07-18 23:38:25 +0530

    added zod usermodel and balanceModel

--- src/db.ts
+++ src/db.ts
@@ -1,12 +1,32 @@
-import mongoose from "mongoose";
+import mongoose, { model, Schema } from "mongoose";
+const ObjectId = Schema.ObjectId;
 
-async function connection(dataBaseURL: string){
-    try{
-        await  mongoose.connect(dataBaseURL)
+async function connection(dataBaseURL: string) {
+    try {
+        await mongoose.connect(dataBaseURL)
         console.log('DataBase is connected')
 
-    }catch{
+    } catch {
         console.error
     }
 }
-export {connection}
+
+const userSchema = new Schema({
+    googleId: { type: Number, unique: true, required: true, id: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    age: { type: Number, required: true },
+    userName: { type: String, required: true },
+    password: { type: String, required: true },
+
+})
+
+const balanceSchema = new Schema({
+    balanceId: { type: ObjectId, required: true, unique: true },
+    userId: { type: Number, required: true, unique: true, ref: 'userSchema' },
+    amount: { type: Number }
+})
+
+const balanceModel = model('balance', balanceSchema)
+const userModel = model('User', userSchema)
+export { connection, userModel , balanceModel}
